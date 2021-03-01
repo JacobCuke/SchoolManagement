7110abd299fa16b22383b2fdc6fedb4612da797a
Jacob Cuke <jacobcuke@gmail.com>
2021-03-01 14:19:17 -0700

    Add school application and basic config

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'school.apps.SchoolConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
