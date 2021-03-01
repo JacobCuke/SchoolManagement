5a0354a4b995faa370a16bc42dbd46084f8358fe
Jacob Cuke <jacobcuke@gmail.com>
2021-03-01 16:13:10 -0700

    Display models as table on admin page

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import Profile
+from .models import (
+    Profile,
+    Student,
+    Instructor
+)
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('user', 'birth_date', 'address')
+
+class StudentAdmin(admin.ModelAdmin):
+    model = Student
+    list_display = ('user', 'year', 'student_id_no')
+
+class InstructorAdmin(admin.ModelAdmin):
+    model = Instructor
+    list_display = ('user', 'start_date', 'office_number', 'office_phone_number')
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Instructor, InstructorAdmin)
--- users/models.py
+++ users/models.py
@@ -5,3 +5,34 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=True)
     address = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile' 
+
+
+class Student(models.Model):
+    GRADE_TEN = '10'
+    GRADE_ELEVEN = '11'
+    GRADE_TWELVE = '12'
+    YEAR_CHOICES = [
+        (GRADE_TEN, '10th Grade'),
+        (GRADE_ELEVEN, '11th Grade'),
+        (GRADE_TWELVE, '12th Grade')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    year = models.CharField(max_length=2, choices=YEAR_CHOICES)
+    student_id_no = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name} ({self.student_id_no})'
+
+
+class Instructor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    start_date = models.DateField()
+    office_number = models.CharField(max_length=10, blank=True)
+    office_phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
