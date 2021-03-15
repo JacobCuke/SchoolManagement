783c1a9ac7a4d8448edf48693b074d817be8a66f
Jacob Cuke <jacobcuke@gmail.com>
2021-03-15 12:53:05 -0600

    Add basic user dashboard logic

--- school/urls.py
+++ school/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import (
+    DashboardListView
+)
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/<str:username>/', DashboardListView.as_view(), name='user-dashboard'),
 ]
--- school/views.py
+++ school/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Course, EnrolledIn, AssistsIn
+from users.models import Student
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 def home(request) :
-    return render(request, 'school/home.html')
+    if (request.user.is_authenticated):
+        return redirect('user-dashboard', username=request.user.username)
+    else:
+        return render(request, 'school/home.html')
+
+
+def dashboard(request):
+    if (request.user.is_authenticated):
+        return redirect('user-dashboard', username=request.user.username)
+    else:
+        return render(request, 'login')
+
+
+class DashboardListView(ListView):
+    model = Course
+    template_name = 'school/dashboard.html'
+    context_object_name = 'courses'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        queryset = {}
+
+        student = Student.objects.filter(user=user).first()
+        if student:
+            enrollments = EnrolledIn.objects.filter(student=student).values('course')
+            queryset['enrolled_courses'] = Course.objects.filter(id__in=enrollments)
+
+            assists = AssistsIn.objects.filter(student=student).values('course')
+            queryset['assisted_courses'] = Course.objects.filter(id__in=assists)
+
+        return queryset
