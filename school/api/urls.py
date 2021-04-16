19d96ace51806f1b38c6925fecc6a6219e52a722
Kendrick Ankamah <kendrick.ankamah@ucalgary.ca>
2021-04-16 11:56:20 -0600

    Added APIs for TAs

--- school/api/urls.py
+++ school/api/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('courses/<int:pk>/', views.course_detail),
     path('courses/<int:course_id>/lectures/', views.lecture_list),
     path('courses/<int:course_id>/assignments/', views.assignment_list),
-    path('courses/<int:course_id>/enrollments/', views.enrollment_list)
+    path('courses/<int:course_id>/enrollments/', views.enrollment_list),
+    path('courses/<int:course_id>/enrollments/<int:student_id>/', views.enroll_detail),
+    path('courses/<int:course_id>/assistants/', views.ta_list),
+    path('courses/<int:course_id>/assistants/<int:student_id>/', views.ta_detail)
 ]
